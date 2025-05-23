8aa83e8b01bb026a2a31741822b76c8064ce48b6
Andrew Chen <andrewc.thirtysix@gmail.com>
2025-05-22 21:00:34 -0400

    update

--- musicPlayer.py
+++ musicPlayer.py
@@ -147,12 +147,13 @@ class MusicPlayer(commands.Cog):
                 # if bot is in different vc than user
                 await voice_client.move_to(voice_channel)
 
+            COOKIES_PATH = os.path.join(os.path.dirname(__file__), 'cookies.txt')
             YDL_OPTIONS = {
                 'format': 'bestaudio/best',
                 'extract_flat': True,
                 'quiet': True,
                 'no_warnings': True,
-                'cookies': 'cookies.txt'
+                'cookies': COOKIES_PATH
             }
 
             # process youtube video into audio
