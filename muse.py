00d1552693103c30c63e80fd2c7b6077fe3a010c
Andrew Chen <andrewc.thirtysix@gmail.com>
2025-05-23 15:40:15 -0400

    added requirements.txt

--- muse.py
+++ muse.py
@@ -8,6 +8,7 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
+import webserver
 
 # Load .env variables
 load_dotenv()
@@ -32,6 +33,7 @@ async def main():
     async with bot:
         await bot.load_extension("musicPlayer")
         await bot.load_extension("utils")
+        webserver.keep_alive()
         await bot.start(TOKEN)
 
 # requires bot token to run
